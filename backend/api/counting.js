3c907797833ef2cfed86f0fec80f17c08c342e85
Sean Leong <seanleongsr@gmail.com>
2021-06-09 14:11:08 +0800

    updated the check plan method and submit method in userctrl

--- backend/api/counting.js
+++ backend/api/counting.js
@@ -1,11 +1,17 @@
 //contains functions that are used in checking of user study plan
 
-
+/**
+ * 
+ * @param {*} ele the modules of the user's study plan
+ * @param {*} check the modules that are in the criteria for a specific major/specialisation
+ * @param {*} num the minimum number of modules in ele that have to be in the check
+ * @returns 
+ */
 function least(ele, check, num) {
     let count = 0;
     var result = {"mod": []};
     for (var y = 0; y < check.length; y++) {
-        if (ele.indexOf(check[y]) > -1) {
+        if (ele.indexOf(check[y].toUpperCase()) > -1) {
             result.mod.push(check[y])
             count += 1;
             if (count >= num) {
--- backend/api/user.controller.js
+++ backend/api/user.controller.js
@@ -3,63 +3,87 @@ const criteriaDAO = require("../dao/criteriaDAO")
 const least = require("./counting")
 
 class userController {
-    /**
-      * @param {http request} req 
-      * @param {http response} res 
-      * this method only works if request body is in json format
-      */
-    static async apiSubmitPlan(req, res) {
-        let submission = req.body;
-        const success = await sampleDAO.addPlan(submission);
-        if (success) {
-            res.send(true);
-        }
-    }
 
     static async apiCheckPlan(req, res) {
         let plan = req.query.y1s1.concat(req.query.y1s2, req.query.y2s1, req.query.y2s2, req.query.y3s1, req.query.y3s2, req.query.y4s1, req.query.y4s2)
-
         let filters = { "major": req.query.major };
         if ("specialisation" in req.query) {
             filters.specialisation = req.query.specialisation;
         }
+        console.log(filters);
         let crit = await criteriaDAO.getCriteria(filters);
-        let answer = {};
-
-        if (crit.core.every(x => plan.indexOf(x) > -1)) {
-            answer.core = true
-            console.log("all core mods present")
+        if (crit == null) {
+            res.send("No criteria found for this major");
+            return;
+        }
+        let answer = {"core":{}};
+        let missing = { "arr": [], "pass": true };
+        for (var y = 0; y < crit.core.length; y++) {
+                if (!(plan.indexOf(crit.core[y]) > -1)) {
+                    missing.arr.push(crit.core[y]);
+                    missing.pass = false;
+            };
+        };
+        if (missing.pass == true) {
+            answer.core.pass = true;
+            answer.pass = true;
+            console.log("all core mods present");
         }
         else {
-            answer.core = false
-            console.log("missing core mods")
+            answer.core.pass = false;
+            answer.core.modules = missing.arr;
         }
-
 
         if (crit.set1) {
             answer.set1 = least(plan, crit.set1, 2);
             answer.set2 = least(plan, crit.set2, 3);
+            if (!(answer.set1.pass || answer.set2.pass)) {
+                answer.pass = false;
+            }
         }
 
-        if (parseInt(req.body.totalmc) < 160) {
-            answer.mc = false;
+        if (parseInt(req.query.totalmc) >= 160) {
+            answer.mc = true;
         }
         else {
-            answer.mc = true;
+            answer.mc = false;
+            answer.pass = false;
+        }
+
+        if (req.query.submit) {
+            return answer;
         }
         console.log(answer);
         res.send(answer);
+        return answer;
+    }
+
+    static async apiSubmitPlan(req, res) {
+        var check = await userController.apiCheckPlan(req, res);
+        if (check.pass) {
+            let submission = req.query;
+            delete submission.submit;
+            console.log(submission);
+            const success = await sampleDAO.addPlan(submission);
+            if (success) {
+                res.send(true);
+            }
+        }
+        else {
+            console.log("Plan does not pass checks");
+            res.send(false);
+        }
     }
 
     static async apiGetModules(req, res) {
         let filters = { "major": req.query.major };
         if ("specialisation" in req.query) {
             filters.specialisation = req.query.specialisation;
-        }
+        };
         let crit = await criteriaDAO.getCriteria(filters);
         console.log(crit);
         res.json(crit);
     }
 }
 
-module.exports = userController
+module.exports = userController;
